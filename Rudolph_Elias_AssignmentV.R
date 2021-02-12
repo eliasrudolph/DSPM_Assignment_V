bc0194841433d89f4ba637f3c99aa76d7fd3dbdc
Elias Rudolph <elias.rudolph@student.uni-tuebingen.de>
2021-02-12 17:44:32 +0100

    Initialize the session and load necessary packages.

---
+++ Rudolph_Elias_AssignmentV.R
@@ -0,0 +1,13 @@
+rm(list = ls())
+
+if (!require("jsonlite")) install.packages("jsonlite")
+if (!require("httr")) install.packages("httr")
+if (!require("rlist")) install.packages("rlist")
+if (!require("tidyverse")) install.packages("tidyverse")
+if (!require("naniar")) install.packages("naniar")
+
+library(jsonlite)
+library(httr)
+library(rlist)
+library(tidyverse)
+library(naniar)
